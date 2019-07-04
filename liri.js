14d8b3e09e15d8319cc30988bf4f0f5d34629bfc
Kip Bradley Smith <ramblekb@gmail.com>
2019-07-04 18:39:38 -0500

    spotify keys added

---
+++ liri.js
@@ -0,0 +1,15 @@
+// add code to read and set any environment variables with the dotenv package:
+require("dotenv").config();
+
+// * You should then be able to access your keys information like so
+
+  var spotify = new Spotify(keys.spotify);
+
+//   Make it so liri.js can take in one of the following commands:
+//    * `concert-this`
+
+//    * `spotify-this-song`
+
+//    * `movie-this`
+
+//    * `do-what-it-says`
